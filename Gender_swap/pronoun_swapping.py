287ac2348ca24f74a805d4099c7863567aa72c44
songbird175 <celina.bekins@students.olin.edu>
2016-11-02 02:45:58 -0400

    Story is now a list

--- Gender_swap/pronoun_swapping.py
+++ Gender_swap/pronoun_swapping.py
@@ -4,54 +4,114 @@ Scount = 0
 Tcount = 0
 Acount = 0
 
+story = ["Far beneath the ocean waves, in the land of the merpeople, lived a lovely young ", "mermaid ", "named Ariel. ", "Her ",
+"home was an undersea palace. Although it was quite magnificent, ", "she ", "hardly ever spent time there. ", "She ", "preferred to go exploring with ", "her ",
+"little friend Flounder, the fish.\n", "Ariel loved to collect objects from the world above the sea. ", "She ",
+"and Flounder would swim in and out of sunken ships, picking up whatever caught the Little ", "Mermaid's ", "fancy.\n\n",
+"Ariel’s father, King Triton, ruled the undersea kingdom. ", "He ", "was very proud of Ariel’s many gifts, especially ", "her ", "beautiful singing voice. ",
+"He ", "had arranged to give a special concert to show his ", "daughter's ",
+"wonderful talent to all the merfolk.\nAriel’s sisters sang to begin the concert. Then they sang a special song to introduce Ariel.\nBut Ariel wasn’t there!\nTriton was not pleased. How could Ariel have forgotten to show up for ",
+"her ", "own debut?\n\nAs usual, Ariel was off exploring. ", "They ", "had found a sunken ship to play in and had forgotten all about the concert. By the time ",
+"they ", "returned home, the guests had left. Triton was so angry that ", "he ", "ordered Sebastian, a crab who was his court composer, to follow Ariel and keep ",
+"them ", "out of trouble.\n\nLater that day, Ariel slipped away again, with Sebastian following close behind. ", "They ",
+"hadn’t gone far when a huge shadow darkened the waters above.\n“It’s a ship!” ", "she ", "gasped with delight.\n\nAriel swam to the surface to get a closer look. There were humans on board! One of them was very handsome. Ariel heard the others call ",
+"him ", "Prince ", "Eric.\n\nBut as Ariel watched, a sudden storm took them all by surprise. To ", "their ", "horror, the ship began to sink. Most of the people on board were able to climb safely into a lifeboat. But the handsome young ",
+"prince ", "was hit by a falling spar and thrown into the churning water.\n\nAriel knew that humans could not live under the sea. ",
+"They ", "had to save the unconscious ", "prince. ", "There was no time to lose!\n\nAriel darted through the waves to the prince as ",
+"their ", "friend, Scuttle, watched anxiously.\n\nUsing all ", "her ", "strength, Ariel dragged the young ", "prince ", "to shore. Scuttle was waiting for them. The silly bird put ",
+"his ear close to Eric’s boot!\n“Sorry, Ariel, I can’t make out a heartbeat,” said Scuttle.\n\nAriel placed ", "their ", "head on the ", "prince’s ",
+"chest.\n“Oh, ", "he ", "is breathing, Scuttle!” ", "they ", "cried.\n", "She ", "was so happy that ", "they ", "began to sing to the ", "prince. ",
+"At the sound of Ariel’s lovely voice, Eric’s eyelids began to flutter.\n\n“", "He’s ", "going to be all right,” Ariel said gratefully. “Now I’d better get back before ",
+"Father ", "finds out I’ve left.”\nWhen the ", "prince ", "opened ", "his ", "eyes, all ", "he ", "saw was ", "his ", "faithful dog, Max, running up to ",
+"him.\n\n", "Ariel returned home, but ", "she ", "couldn’t forget the human ", "prince. As ", "they ", "sat thinking about ", "him, ",
+"two evil eels named Flotsam and Jetsam watched ", "her ", "with their cunning eyes.\n\n“Come with us,” they coaxed sweetly. “We know someone who can help you.”\nAriel followed the slippery eels. Before ",
+"they ", "knew it, they were inside the deep, dark cave of Ursula, the Sea Witch!\n\nUrsula knew how much Ariel missed Eric. ", "She ",
+"told the young ", "merperson ", "that ", "she ", "could help ", "them ", "visit ", "him ", "– for a price!\n\n“I will make you human so you can visit your young ",
+"man,” ", "Ursula said. “But you must get ", "him ", "to fall in love and kiss you before the sun sets on the third day. If you fail, you will turn back into a ",
+"merperson ", "and return to the sea as my slave!”\nThen Ursula laughed wickedly. “Oh, and one other thing. You must leave your voice with me!”\n\nPoor Ariel! ",
+"They ", "would have done anything to see the young ", "prince ", "again. So ", "they ", "signed the agreement! Then the evil witch locked Ariel’s beautiful voice inside a magic seashell that ",
+"she ", "wore around ", "her ", "neck.\n\nFlounder and Sebastian watched in amazement as Ursula’s magic powers changed Ariel into a human ", "girl. ",
+"Suddenly Ariel could no longer live underwater. Luckily, ", "her ", "two friends rushed ", "them ", "up to the surface so ", "they ",
+"could breathe.\nUrsula cackled with glee. ", "She ", "intended to make sure Ariel did not get ", "their ", "kiss!\n\nOnce Ariel was safe on the beach, Scuttle said, “There’s something different about you, Ariel. I just can’t put my foot on it.”\nAriel showed ",
+"him ", "their ", "two legs. ", "They", " wanted to tell Scuttle about what had happened. But since Ursula had taken ", "her ",
+"voice, Sebastian had to explain.\n\nWhen Ariel tried to stand, ", "they ", "found that balancing on ", "their ", "new legs was harder than it looked. “You can do it,” said Scuttle. “Just watch me.”\nSebastian shook ",
+"his ", "head in disbelief. “How am I going to tell the king about this?” ", "he ", "wondered.\n\nMeanwhile, Eric had not forgotten about the mystery ",
+"person ", "who had saved his life. Although ", "he ", "hadn’t seen ", "her, ", "he ", "had fallen in love with ", "her ", "wonderful voice. Eric would have given ",
+"his ", "entire kingdom to find ", "her! ", "Then, as ", "he ", "was walking with Max, ", "he ", "saw Ariel on the beach.\n“Have we met before?” Eric asked, ",
+"his ", "heart skipping a beat. But ", "his ", "hopes were soon dashed when ", "he ", "found that Ariel could not speak. ", "They ", "can’t be the one, ", "he ",
+"thought.\n\nStill, the ", "prince ", "felt sorry for Ariel, so ", "he ", "brought ", "them ", "to ", "his ", "castle. ", "His ", "servants fed ", "her ",
+"and gave ", "them ", "nice clothes to wear.\n\nThe next day, the ", "prince ", "took Ariel for a coach ride through ", "his ", "kingdom. “", "She's ",
+"so lovely,” Eric said to ", "himself. ", "“If only ", "she ", "were the ", "person ", "I’ve been searching for!”\n\nThe next day, the ", "prince ",
+"took Ariel for a boat ride.\nThere isn’t much time, thought Ariel. If ", "he ", "doesn’t kiss me soon, I will become Ursula’s slave.\nAlthough Ariel didn’t know it, the ",
+"prince ", "was falling in love and was just about to kiss ", "her.\n\n", "But Flotsam and Jetsam had been watching them. They tipped the boat over and sent the young couple tumbling into the water before Ariel could get ",
+"her ", "kiss!\n\nUrsula had been watching the couple in ", "her ", "magic bubble. ", "She ", "decided to trick Eric so Ariel would never get ", "her ",
+"kiss. The witch transformed ", "herself ", "into a beautiful ", "girl.\n", "With Ariel’s voice locked inside the seashell necklace, Ursula would sound just like Eric’s true love!\n\nThe following day, as Ursula walked along the beach, the sound of Ariel’s beautiful voice filled the air. Eric could hardly believe ",
+"his ", "ears! The ", "girl ", "who had saved ", "his ", "life had finally come back to ", "him!\n", "Quickly, the Sea Witch put a spell on Eric, and ", "he ",
+"decided to marry ", "her ", "right away.\n\nUrsula’s nasty trick had worked. ", "She ", "and Eric were to be married on ", "his ", "ship that very day!\nIn ",
+"her ", "cabin, Ursula laughed as ", "she ", "looked at ", "her ", "true reflection in the mirror. Luckily, Scuttle saw Ursula’s real reflection, too.\n\nScuttle and Flounder rushed off to find Ariel and tell ",
+"them ", "all about the Sea Witch’s latest trick.\n“Don’t worry, Ariel. We’ll help you!” said Scuttle, as Flounder pulled ", "them ", "out to the ship. Then Scuttle flew to the ship.\n\nWith the help of sea lions, pelicans, and other birds, Scuttle stopped the wedding. As Ariel climbed over the railing, the birds tore at Ursula’s clothing and pulled ",
+"her ", "hair. The seashell fell from Ursula’s neck. When it broke, Ariel’s voice returned to ", "her ", "instantly. Ariel called out to warn the ", "prince.\n\n",
+"Then Eric knew ", "he ", "had been tricked. But time had run out. The sun had started to set, and Ariel was already turning back into a ", "merperson.\n",
+"“You belong to me now!” gloated Ursula, who had turned back into ", "her ", "mean, ugly self. ", "She ", "grabbed Ariel and slithered into the sea.\n\nBut Eric was not about to let Ariel slip away this time! Grabbing a harpoon, ",
+"he ", "dived into the sea and hurled it at the Sea Witch!\n\nThat made Ursula angrier than ever! ", "She ", "transformed ", "herself ",
+"into a huge monster and tried to crush poor Ariel and the ", "prince ", "with ", "her ", "black tentacles.\nEric clutched the Little ", "Mermaid ",
+"to ", "him, ", "trying to protect ", "her ", "from the witch’s fury.\n\nEric knew ", "he ", "had to do something, and fast! Climbing aboard a ship that Ursula’s angry thrashing had raised from the bottom of the sea, ",
+"he ", "grabbed the wheel. ", "He ", "aimed the ship’s prow at Ursula and sailed full speed ahead! As the boat struck the witch, ", "she ",
+"let out a terrible scream. Then ", "she ", "was gone.\n\nEveryone was glad the Sea Witch was gone, but Ariel was still unhappy. Even Triton could see that his ",
+"daughter ", "was truly in love. So, using ", "his ", "magic powers, ", "he ", "granted Ariel ", "their ", "dearest wish – ", "he ", "made ", "them ",
+"human again.\n\nTriton watched as Eric and Ariel walked back to the castle.\n“I’m going to miss ", "her,” ", "King ", "Triton said to Sebastian, “but I know they’ll be happy.”\nSoon after that, Eric and Ariel were married in a splendid shipboard wedding with all the merpeople looking on. And just as ",
+"King ", "Triton had predicted, they lived happily ever after!\n"]
+
 for number in range(0, iterations):
 	number += 1
 
 	working_file = open("Rewrite_%s" % str(number), "w")
 
 	if number % 3 == 1:
-		Ursula2_pro = "she"
+		Ursula2_pro = "she "
 		Ucount += 1
 
 		if Ucount % 3 == 1:
-			Ursula_pro = "she"
+			Ursula_pro = "she "
 			Scount += 1
 
 			if Scount % 3 == 1:
-				Scuttle_pro = "she"
+				Scuttle_pro = "she "
 				Tcount += 1
 
 				if Tcount % 3 == 1:
-					Triton_pro = "she"
+					Triton_pro = "she "
 					Acount += 1
 
 					if Acount % 3 == 1:
-						Ariel_pro = "she"
+						Ariel_pro = "she "
 					elif Acount % 3 == 2:
-						Ariel_pro = "he"
+						Ariel_pro = "he "
 					elif Acount % 3 == 0:
-						Ariel_pro = "they"
+						Ariel_pro = "they "
 
 				elif Tcount % 3 == 2:
-					Triton_pro = "he"
+					Triton_pro = "he "
 				elif Tcount % 3 == 0:
-					Triton_pro = "they"
+					Triton_pro = "they "
 
 			elif Scount % 3 == 2:
-				Scuttle_pro = "he"
+				Scuttle_pro = "he "
 			elif Scount % 3 == 0:
-				Scuttle_pro = "they"
+				Scuttle_pro = "they "
 
 		elif Ucount % 3 == 2:
-			Ursula_pro = "he"
+			Ursula_pro = "he "
 		elif Ucount % 3 == 0:
-			Ursula_pro = "they"
+			Ursula_pro = "they "
 
 	elif number % 3 == 2:
-		Ursula2_pro = "he"
+		Ursula2_pro = "he "
 	elif number % 3 == 0:
-		Ursula2_pro = "they"
+		Ursula2_pro = "they "
 
 	working_file.write("Version %s" % number + "\n")
-	working_file.write("Ariel: %s," % Acount + " Triton: %s," % Triton_pro + " Scuttle: %s," % Scuttle_pro + " Ursula: %s," % Ursula_pro + " Transformed Ursula: %s" % Ursula2_pro)
+	working_file.write("Ariel: %s," % Acount + " Triton: %s," % Triton_pro + " Scuttle: %s," % Scuttle_pro +
+		" Ursula: %s," % Ursula_pro + " Transformed Ursula: %s\n" % Ursula2_pro)
+	working_file.write("".join(story))
 	working_file.close()
