99e07f6e7d15fe4418045a61da32820d44f52e43
songbird175 <celina.bekins@students.olin.edu>
2016-11-01 23:26:51 -0400

    Script effectively initializes files with pronouns

--- writing_test.py
+++ writing_test.py
@@ -1,2 +1,7 @@
-working_file = open("Python_test.txt", 'w')
-working_file.write("This is a test.")
+for i in range(0,5):
+	i += 1
+
+	working_file = open("Python_test%s.txt" % i, 'w')
+	working_file.write("This is a test. \n")
+	working_file.write("Test number two")
+	
